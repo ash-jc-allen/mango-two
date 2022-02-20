32415efca513dad0ba4352c9391320aedc3127c9
Ash Allen <mail@ashallendesign.co.uk>
2022-02-20 22:19:38 +0000

    Updated layout. Added favicon.

--- resources/views/components/home-page-button-link.blade.php
+++ resources/views/components/home-page-button-link.blade.php
@@ -2,7 +2,7 @@
     'href',
 ])
 
-<div class="bg-gradient-to-t from-dark-orange to-light-orange p-0.5 rounded">
+<div class="bg-gradient-to-t from-dark-orange to-light-orange p-0.5 rounded mt-4">
     <a href="{{ $href }}" {{ $attributes->merge(['class' => 'inline-block bg-really-dark text-white py-3 px-6 rounded']) }}>
         {{ $slot }}
     </a>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,10 @@
 <html lang="en">
 <head>
     <title>Mango Two | Privacy-First, Open Source URL Shortener</title>
-    <link href="/css/app.css" rel="stylesheet">
+
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 </head>
 <body class="bg-dark min-h-screen">
 <header class="pt-6 max-w-7xl mx-auto px-8">
@@ -19,7 +22,7 @@
         eligendi est nisi nulla qui similique. Corporis perspiciatis, voluptatum.
     </p>
 
-    <div class="mt-14 flex justify-center space-x-4">
+    <div class="mt-14 flex justify-center md:space-x-4 flex-wrap">
         <x-home-page-button-link href="#">
             <img src="/img/chrome-logo.png" alt="Firefox logo" class="inline w-5 h-5 mr-2 align-middle"> Chrome coming soon...
         </x-home-page-button-link>
@@ -30,12 +33,12 @@
     </div>
 
     <div class="relative py-16 sm:py-24 lg:py-32 mt-32">
-        <div class="mx-auto  px-4 text-center sm:px-6 lg:px-8">
+        <div class="mx-auto px-4 text-center sm:px-6 lg:px-8">
             <h2 class="text-base font-semibold tracking-wider text-light-orange uppercase">Deploy faster</h2>
             <p class="mt-2 text-3xl font-extrabold text-gray-50 tracking-tight sm:text-4xl">Everything you need to deploy your app</p>
             <p class="mt-5 max-w-prose mx-auto text-xl text-gray-500">Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.</p>
             <div class="mt-12">
-                <div class="grid gap-8 sm:grid-cols-2 grid-cols-3">
+                <div class="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
                     <x-feature title="Privacy First">
                         <x-slot name="text">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, quo?
@@ -81,7 +84,7 @@
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="w-full text-light-orange bg-dark">
     <path fill="currentColor" fill-opacity="1" d="M0,128L80,128C160,128,320,128,480,160C640,192,800,256,960,261.3C1120,267,1280,213,1360,186.7L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
 </svg>
-<footer class="text-dark text-center bg-light-orange pb-6">
+<footer class="text-dark text-center bg-light-orange pb-6 -mt-2">
     <p>Powered by <a href="https://ashallendesign.co.uk" class="font-extrabold">Ash Allen Design</a> 🚀</p>
 </footer>
 </body>
