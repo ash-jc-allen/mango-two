d07a8244b3bb2e148a36ea2518c5577d40b7e955
Ash Allen <mail@ashallendesign.co.uk>
2022-04-15 23:20:12 +0100

    Updates to make testing easier.

--- app/Http/Resources/ShortURLResource.php
+++ app/Http/Resources/ShortURLResource.php
@@ -9,17 +9,17 @@ class ShortURLResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
         return [
-            'id'                   => $this->id,
-            'destination_url'      => $this->destination_url,
-            'url'                  => $this->default_short_url,
+            'id' => $this->id,
+            'destination_url' => $this->destination_url,
+            'url' => $this->default_short_url,
             'redirect_status_code' => $this->redirect_status_code,
-            'created_at'           => $this->created_at,
+            'created_at' => $this->created_at,
         ];
     }
 }
